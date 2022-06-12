e4b4472e9eb40fe6e43457388a0046e4dcc9cd45
Jesus Vallez Gomis <jesusvg91@gmail.com>
2022-06-12 22:24:24 +0200

    chore(loader): remove image loader

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const cloudinaryBaseUrl = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/`
-
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com'],
-    formats: ['image/webp'],
-    loader: 'cloudinary',
-    path: cloudinaryBaseUrl,
-  },
 }
